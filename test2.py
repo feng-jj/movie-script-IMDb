b8f73b697bcaaf7b12615d87d112142f8194e4b3
feng-jj <jesse.j.feng@vanderbilt.edu>
2020-08-15 16:10:07 -0700

    finished word frequency

--- test2.py
+++ test2.py
@@ -1,15 +1,24 @@
+import nltk
 import requests
 from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
 from nltk.tokenize import PunktSentenceTokenizer
-
+from nltk.tag import pos_tag
+from nltk import FreqDist
 '''
-    This file is used to test natural language processing 
+    This file is used to test natural language processing
 '''
 
 url = 'https://www.imsdb.com/scripts/Good-Will-Hunting.html'
 
+
+
+"""
+    Separates the url containing the script into a pair with dialogue and titles (i.e. names of characters, camera
+    movement instructions such as FADE IN)
+"""
 
 def getCorp(url):
     r = requests.get(url)
@@ -36,26 +45,38 @@ def getCorp(url):
     for y in range(0, len(titles)):
         allTitles += titles[y]
 
-    # exploring tagging parts of speech
+    pair = (fullScript, allTitles)
 
-    tokenized = PunktSentenceTokenizer
-
-    for i in fullScript :
-        words = nltk
+    return pair
 
 
-    # # titles1 contains all the things we want to separate from the dialogue
-    # titles1 = word_tokenize(allTitles)
-    #
-    # #first extending all of the title and setting descriptions
-    # stop_words = [stopwords.words("english")]
-    # stop_words.extend(titles1)
-    # words = word_tokenize(fullScript)
-    #
-    # # extracting only the dialogue and plot descriptions
-    # filtered_text = [w for w in words if not w in stop_words and not w in titles1]
-    # print(filtered_text)
+"""
+    This is used to tag the part of speech that each word is in the movie script
+"""
+def token(fullScript, allTitles) :
+    # titles1 contains all the things we want to separate from the dialogue
+    titles1 = word_tokenize(allTitles)
+
+    # first extending all of the title and setting descriptions to the list of stop words
+    stop_words = [stopwords.words("english")]
+    stop_words.extend(titles1)
+    update_list = ['the', 'and', 'you']
+    #stop_words.extend('to', 'the')
+
+    words = word_tokenize(fullScript)
+    # extracting only the dialogue and plot descriptions
+    filtered_text = [w for w in words if not w in stop_words and not w in titles1]
+
+    # this part tags the part of speech of each word inside the movie script
+    tagged_text = pos_tag(filtered_text)
+    #print(tagged_text)
+    return filtered_text
+
+
 
 
 if __name__ == '__main__':
-    getCorp(url)
+    info = getCorp(url)
+    script_words = token(info[0], info[1])
+    freq_dist = FreqDist(script_words)
+    print(freq_dist.most_common(100))
