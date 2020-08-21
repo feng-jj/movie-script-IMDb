3f6e8c8ced0522ae8add7ffbaf8e4318d1c1c58c
feng-jj <jesse.j.feng@vanderbilt.edu>
2020-08-20 21:25:22 -0700

    started sentiment analysis inputting

---
+++ nltkTest.py
@@ -0,0 +1,114 @@
+import nltk
+import requests
+from bs4 import BeautifulSoup
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import PunktSentenceTokenizer
+from nltk.tag import pos_tag
+from nltk import FreqDist
+from nrclex import NRCLex
+'''
+    This file is used to test natural language processing
+'''
+
+url = 'https://www.imsdb.com/scripts/Good-Will-Hunting.html'
+
+
+"""
+    Separates the url containing the script into a pair with dialogue and titles (i.e. names of characters, camera
+    movement instructions such as FADE IN)
+"""
+
+def getCorp(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    # want to separate the titles (i.e. character names) with the dialogue.
+    corpora = soup.find_all("pre")
+    title_info = soup.find_all("b")
+
+    # extracting our text
+    # script contains the dialogue and scenic descriptions in the script. The Titles contains title,
+    # name, camera movement, and setting descriptions. This way we can process dialogue separately
+    script = [w for w in corpora]
+    titles = [names.text for names in title_info]
+
+    # contains the dialogue of our script
+    fullScript = ''
+    allTitles = ''
+
+    for x in range(0, len(script)):
+        # print(script[x].text)
+        fullScript += (script[x].text)
+
+    for y in range(0, len(titles)):
+        allTitles += titles[y]
+
+    pair = (fullScript, allTitles)
+
+    return pair
+
+"""
+    This method utilizes the NRCLex library that processes the emotional counts
+    of the movie script.
+"""
+def emotions(script) :
+    totalScript = script[0] + script[1]
+    text_object = NRCLex(totalScript);
+    #Return words list.
+
+    text_object.words
+
+    #Return sentences list.
+
+    text_object.sentences
+
+    #Return affect list.
+
+    text_object.affect_list
+
+    #Return affect dictionary.
+
+    text_object.affect_dict
+
+    #Return raw emotional counts. This one gives the actual frequency
+    #of each emotion
+
+    print(text_object.raw_emotion_scores)
+
+    #Return highest emotions.
+
+    print(text_object.top_emotions)
+
+    print(text_object.affect_frequencies)
+
+"""
+    This is used to tag the part of speech that each word is in the movie script
+"""
+def token(fullScript, allTitles) :
+    # titles1 contains all the things we want to separate from the dialogue
+    titles1 = word_tokenize(allTitles)
+
+    # first extending all of the title and setting descriptions to the list of stop words
+    stop_words = [stopwords.words("english")]
+    stop_words.extend(titles1)
+    update_list = ['the', 'and', 'you']
+    #stop_words.extend('to', 'the')
+
+    words = word_tokenize(fullScript)
+    # extracting only the dialogue and plot descriptions
+    filtered_text = [w for w in words if not w in stop_words and not w in titles1]
+
+    # this part tags the part of speech of each word inside the movie script
+    tagged_text = pos_tag(filtered_text)
+    #print(tagged_text)
+    return filtered_text
+
+
+if __name__ == '__main__':
+    info = getCorp(url)
+    script_words = token(info[0], info[1])
+    freq_dist = FreqDist(script_words)
+    stuff = emotions(info)
+    #print(stuff)
---
+++ tests/test.py
@@ -0,0 +1,95 @@
+import requests
+import numpy as np
+from IMDb import IMDb
+from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+
+""" 
+    This file is used to test out functionality and new added features.
+"""
+#url = 'https://www.imdb.com/search/title/?title_type=feature&num_votes=25000,&genres=animation&sort=boxoffice_gross_us,desc'
+
+
+titles = []
+ratings = []
+amount = []
+
+def genre(aGenre):
+    url = 'https://www.imdb.com/search/title/?title_type=feature&num_votes=25000,&genres='
+    # Change this based on how you want to sort the website. For now it will be based on box office, since
+    # some movies actually don't have box office data.
+    url_completed = '&sort=boxoffice_gross_us,desc'
+
+    # a is the temp url that is created. b is the starting number for the page
+    url_pages = lambda a, b: a + '&start=' + str(b) + '&ref_=adv_nxt'
+
+    #if aGenre == 'action':
+    temp = url + aGenre + url_completed
+    main(temp)
+    numTitles = numberOfTitles(temp)
+    counter = 1
+    while numTitles > 50:
+        counter += 50
+        anew = url_pages(temp, counter)
+        main(anew)
+        numTitles -= 50
+    if (numTitles > 0) :
+        counter += 50
+        anew = url_pages(temp, counter)
+        main(anew)
+    return
+
+def main(url):
+    r = requests.get(url)
+
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    # getting all of our necessary data
+    # movieRating = soup.select('div.inline-block.ratings-imdb-rating')
+    # theRating = movieRating.find('strong').text
+    movieTitles = soup.select('h3.lister-item-header a')
+    movieRating = soup.select('div.inline-block.ratings-imdb-rating')
+    grossAmount = soup.select('p.sort-num_votes-visible span[name = nv]')
+
+    # obtain all of the necessary data needed
+    titles.extend([title.text for title in movieTitles])
+    ratings.extend([float(rating['data-value']) for rating in movieRating])
+    # had to separate the right data values, as votes and gross amount were both under the name nv in span
+    amount.extend(int(gross['data-value'].replace(',', '')) for votes, gross in pairwise(grossAmount))
+
+    #print(numTitles)
+
+    # records = []
+    # for (a, b, c) in zip(titles, ratings, amount):
+    #     records.append((a, b, c))
+    #
+    # df = pd.DataFrame(records, columns=('Title', 'Rating', 'Net Gross'))
+    # df.head()
+    # df.tail()
+    # df.to_csv('theData.csv', index=False, encoding='utf-8')
+
+def pairwise(iterable):
+    a = iter(iterable)
+    return zip(a, a)
+
+
+def numberOfTitles(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    numberTitles = soup.find_all("div", class_="desc")
+    test = numberTitles[0].text[6:-1]
+    Titles = int(''.join(filter(str.isdigit, test)))
+    return Titles
+    #numTitles = numbTitles[0]
+
+if __name__ == '__main__' :
+    genre('western')
+    x = np.array([ratings])
+    y = np.array([amount])
+    plt.plot(x,y)
+    plt.xlabel('IMDb Rating')
+    plt.ylabel('Gross Amount')
+    # print(titles)
+    # print(ratings)
+    # print(amount)
+    # print(len(titles))
